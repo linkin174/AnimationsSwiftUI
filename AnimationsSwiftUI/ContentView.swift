1ccda2a81677ca3270ab3c9e151920ad25bf97f1
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-29 22:01:18 +0500

    Initial Commit

---
+++ AnimationsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationsSwiftUI
+//
+//  Created by Aleksandr Kretov on 29.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
