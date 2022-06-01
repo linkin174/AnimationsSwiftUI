822b329388de670399573ca8bcbbdacf212d6868
Aleksandr Kretov <kretov.1988@gmail.com>
2022-06-01 12:24:34 +0500

    Tuples

--- AnimationsSwiftUI/ContentView.swift
+++ AnimationsSwiftUI/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var toggleAnimation = [false, false, false, false]
+    @State private var textAnimating: (greet: Bool, swift: Bool) = (false, false)
     @State private var isSelfDissmissed = false
+    @State private var rectParamteres: (opacity: Double, scale: Double) = (0, 5.0)
+    @State private var birdParameters: (opacity: Double, scale: Double) = (0, 5.0)
+    @State private var delay: Double = 0.5
 
     private let bounds = UIScreen.main.bounds
 
@@ -21,13 +24,13 @@ struct ContentView: View {
                     .foregroundColor(.red)
                     .frame(width: 250, height: 250)
                     .shadow(color: .gray, radius: 3, x: 5, y: 5)
-                    .scaleEffect(toggleAnimation[0] ? 1 : 3)
-                    .opacity(toggleAnimation[0] ? 1 : 0)
+                    .scaleEffect(rectParamteres.scale)
+                    .opacity(rectParamteres.opacity)
                 SwiftView()
                     .foregroundColor(.white)
                     .frame(width: 200, height: 200)
-                    .opacity(toggleAnimation[1] ? 1 : 0)
-                    .scaleEffect(toggleAnimation[1] ? 1 : 5)
+                    .opacity(birdParameters.opacity)
+                    .scaleEffect(birdParameters.scale)
             }
             .onTapGesture {
                 isSelfDissmissed.toggle()
@@ -37,33 +40,37 @@ struct ContentView: View {
             HStack {
                 Text("Welcome to")
                     .font(.title).bold()
-                    .offset(x: toggleAnimation[2] ? 0 : -bounds.midX)
-                    .opacity(toggleAnimation[2] ? 1 : 0)
+                    .offset(x: textAnimating.greet ? 0 : -bounds.midX)
+                    .opacity(textAnimating.greet ? 1 : 0)
                 ShimmeringText(textColor: .red, shimColor: .white.opacity(0.7), text: "SwiftUI 3")
                     .font(.title.bold())
-                    .opacity(toggleAnimation[3] ? 1 : 0)
+                    .opacity(textAnimating.swift ? 1 : 0)
             }
             Spacer()
             Text("Tap on Swift to go back...")
                 .foregroundColor(.gray)
-                .opacity(toggleAnimation[3] ? 0.7 : 0)
+                .opacity(textAnimating.swift ? 0.7 : 0)
         }
         .padding()
         .onAppear {
-            withAnimation(Animation.spring(response: 0.5, dampingFraction: 0.7).delay(0.5)) {
-                toggleAnimation[0].toggle()
+            withAnimation(Animation.spring(response: 0.5, dampingFraction: 0.7).delay(delay)) {
+                rectParamteres.opacity = 1
+                rectParamteres.scale = 1
+                delay += 0.5
+            }
+            withAnimation(Animation.spring(response: 0.9, dampingFraction: 0.6).delay(delay)) {
+                birdParameters.opacity = 1
+                birdParameters.scale = 1
+                delay += 0.5
+            }
 
-                withAnimation(Animation.spring(response: 0.9, dampingFraction: 0.6).delay(1)) {
-                    toggleAnimation[1] = true
-                }
+            withAnimation(.easeInOut(duration: 0.5).delay(delay)) {
+                textAnimating.greet.toggle()
+                delay += 0.5
+            }
 
-                withAnimation(.easeInOut(duration: 0.5).delay(1.5)) {
-                    toggleAnimation[2] = true
-                }
-
-                withAnimation(.easeInOut(duration: 1).delay(2)) {
-                    toggleAnimation[3] = true
-                }
+            withAnimation(.easeInOut(duration: 1).delay(delay)) {
+                textAnimating.swift.toggle()
             }
         }
     }
